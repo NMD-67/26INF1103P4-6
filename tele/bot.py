b4507e99ddb6879ab91d40f4eba92f60a6cbed52
NMD-67 <2603197@sit.singaporetech.edu.sg>
2026-09-25 11:14:41 +0800

    Added testing buttons for profile

--- tele/bot.py
+++ tele/bot.py
@@ -1,4 +1,4 @@
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
 from dotenv import load_dotenv
 from pathlib import Path
@@ -19,8 +19,6 @@ def get_tele_id(update: Update):
 async def send_msg(update: Update, message: str):
     await update.message.reply_text(message)
     return
-            
-    
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await send_msg(update, "Welcome to SITogether!\nUse the /login command to get started!")
@@ -30,7 +28,10 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def login(update: Update, context: ContextTypes.DEFAULT_TYPE):
     tele_id = get_tele_id(update)
-
+    bot_user = get_bot_user(tele_id)
+    if bot_user is not None:
+        await send_msg(update, "You are already logged in!")
+        return
     if len(context.args) != 1:
         await send_msg(update, "Usage: /login <student_id>")
         return
@@ -43,7 +44,6 @@ async def login(update: Update, context: ContextTypes.DEFAULT_TYPE):
     elif response == 500:
         await send_msg(update, "Error occurred while sending OTP. Please try again later.")
     else:
-        save_user(tele_id=tele_id, student_id=student_id)
         await send_msg(update, "An OTP has been sent to your email. Please check your inbox. \nEnter OTP using the /otp command:\n/otp <student_id> <otp>")
 
 async def otp(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -51,15 +51,18 @@ async def otp(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await send_msg(update, "Usage: /otp <student_id> <otp>")
         return
 
+    
     student_id = context.args[0]
     otp = context.args[1]
-
+    tele_id = get_tele_id(update)
     result = validate_otp(student_id=student_id, otp=otp)
     print(f"opt res {result}")
     if result == 200:
+        save_user(tele_id, student_id)
         await send_msg(update, "OTP verified!")
         return
     if result == 201:
+        save_user(tele_id, student_id)
         await send_msg(update, "Account Created!")
         return
     if result == 400:
@@ -99,21 +102,26 @@ async def profile(update: Update, context: ContextTypes.DEFAULT_TYPE):
       reply = "User Details:\n"
       for key, value in user_details.items():
           reply += f"{key}: {value}\n"
-      await send_msg(update, reply)
+
+      keyboard = InlineKeyboardMarkup([
+          [InlineKeyboardButton("Edit bio", callback_data="edit_bio")],
+          [InlineKeyboardButton("Edit hobbies", callback_data="edit_hobbies")]
+      ])
+
+      await update.message.reply_text(reply, reply_markup=keyboard)
     except Exception as e:
         print(f"Error with /profile: {e}")
         await send_msg(update, "An Error occurred, contact nmd_002")
 
-      
-          
-  
-
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await send_msg(update, "COMMANDS\n/login:\t Login to account\notp:\tEnter OTP after /login\n/profile:\tView Profile")
 
 app = ApplicationBuilder().token(os.environ.get("TELE_API_KEY")).build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("login", login))
 app.add_handler(CommandHandler("otp", otp))
 app.add_handler(CommandHandler("profile", profile))
+app.add_handler(CommandHandler("help", help))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
 app.run_polling()
